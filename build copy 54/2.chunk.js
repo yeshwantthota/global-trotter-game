19c1e84421c8b027f8c6d68d1e44636bee8aa22f
yeshwantthota <thotayeshwant@gmail.com>
2025-04-29 17:03:03 +0530

    script updated

---
+++ build copy 54/2.chunk.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpBottomSticky"] = window["webpackJsonpBottomSticky"] || []).push([[2],{
+
+/***/ "./node_modules/@practo/practo-ai/dist/__bundle-8b4f432b-l0sNRNKZ.js":
+/*!***************************************************************************!*\
+  !*** ./node_modules/@practo/practo-ai/dist/__bundle-8b4f432b-l0sNRNKZ.js ***!
+  \***************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n//# sourceMappingURL=__bundle-8b4f432b-l0sNRNKZ.js.map\n\n\n//# sourceURL=webpack://BottomSticky/./node_modules/@practo/practo-ai/dist/__bundle-8b4f432b-l0sNRNKZ.js?");
+
+/***/ })
+
+}]);
